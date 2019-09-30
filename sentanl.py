427c9439aa232cde913dc27c86221b3cbb1e1fd7
yanjh95 <38827628+yanjh95@users.noreply.github.com>
2019-09-30 12:14:10 -0400

    Update sentanl.py

--- sentanl.py
+++ sentanl.py
@@ -1,3 +1,4 @@
+  
 import json
 from google.cloud import language
 from google.cloud.language import enums
@@ -7,16 +8,16 @@ client = language.LanguageServiceClient()
 
 print("===========================================================")
 
-with open('tweetex.json') as f:
+with open('reviews.json') as f:
   data = json.load(f)
   for i in data:
-    text = data[i]["Text"]
+    text = data[i]["Text "]
     location = data[i]["Location"]
 # The text to analyze
     document = types.Document(content=text,type=enums.Document.Type.PLAIN_TEXT)
 
 # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    sentiment= client.analyze_sentiment(document=document).document_sentiment
 
     print('Location: {}'.format(location))
     print('Sentiment: {0:.3f}, {0:.3f} \n'.format(sentiment.score, sentiment.magnitude))
